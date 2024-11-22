7970b6ae6bdc834b04aa0100cec8aaee5a149088
Ariful Islam <arifulislamjoy597.com>
2024-11-22 23:10:05 +0600

    change

--- socket/socket.js
+++ socket/socket.js
@@ -13,12 +13,40 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected.", socket.id);
+let activeUsers = []; // Array to store active user IDs
+io.on("connection", async (socket) => {
+  // console.log("a user connected.", socket.id);
 
+  // Handle user joining with an ID
+  socket.on("user:join", (userId) => {
+    const isUserAlreadyActive = activeUsers.some(
+      (user) => user.userId === userId
+    );
+    if (!isUserAlreadyActive) {
+      activeUsers.push({ userId, socketId: socket.id }); // Add user to active list
+    }
+
+    // console.log("Active users:", activeUsers);
+
+    // Send the updated list of active users to all clients
+    io.emit(
+      "activeUsers:update",
+      activeUsers.map((user) => user.userId)
+    );
+  });
+
+  // Handle user disconnection
   socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
+    // Remove the user from the active users list using their socket ID
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+
+    // console.log("Active users after disconnect:", activeUsers);
+
+    // Send the updated list of active users to all clients
+    io.emit(
+      "activeUsers:update",
+      activeUsers.map((user) => user.userId)
+    );
   });
 });
-
 module.exports = { app, server, io };
